c179688af378dbc8ec153465b463d38025fefd13
BOBO <bohdansheva06@gmail.com>
2023-11-12 19:01:27 +0200

    init

--- src/container/signup/index.js
+++ src/container/signup/index.js
@@ -1,18 +1,111 @@
-class SignupForm {
-  static value = {}
+import {
+  Form,
+  REG_EXP_EMAIL,
+  REG_EXP_PASSWORD,
+} from '../../script/form'
 
-  static validate = (name, value) => {
-    return true
+class SignupForm extends Form {
+  FIELD_NAME = {
+    EMAIL: 'email',
+    PASSWORD: 'password',
+    PASSWORD_AGAIN: 'passwordAgain',
+    ROLE: 'role',
+    IS_COMFIRM: 'isComfirm',
+  }
+  FIELD_ERROR = {
+    IS_EMPTY: 'Write value in field',
+    IS_BIG: 'So long value, remove the excess',
+    EMAIL: 'Write corect value email adress',
+    PASSWORD: 'Password need consist of 8 characters ',
+    PASSWORD_AGAIN:
+      'Your secondary password not similar to the first',
+    NOT_COMFIRM: 'You don`t confirm rules',
+    ROLE: 'Yon don`t choice role',
   }
 
-  static submit = () => {
-    console.log(this.value)
+  validate = (name, value) => {
+    if (String(value).length < 1) {
+      return this.FIELD_ERROR.IS_EMPTY
+    }
+
+    if (String(value).length > 20) {
+      return this.FIELD_ERROR.IS_BIG
+    }
+
+    if (name === this.FIELD_NAME.EMAIL) {
+      if (!REG_EXP_EMAIL.test(String(value))) {
+        return this.FIELD_ERROR.EMAIL
+      }
+    }
+
+    if (name === this.FIELD_NAME.PASSWORD) {
+      if (!REG_EXP_PASSWORD.test(String(value))) {
+        return this.FIELD_ERROR.PASSWORD
+      }
+    }
+
+    if (name === this.FIELD_NAME.PASSWORD_AGAIN) {
+      if (
+        String(value) !==
+        this.value[this.FIELD_NAME.PASSWORD]
+      ) {
+        return this.FIELD_ERROR.PASSWORD_AGAIN
+      }
+    }
+
+    if (name === this.FIELD_NAME.ROLE) {
+      if (isNaN(value)) {
+        return this.FIELD_ERROR.ROLE
+      }
+    }
+
+    if (name === this.FIELD_NAME.IS_COMFIRM) {
+      if (Boolean(value) !== true) {
+        return this.FIELD_ERROR.NOT_COMFIRM
+      }
+    }
   }
 
-  static change = (name, value) => {
-    console.log(name, value)
-    if (this.validate(name, value)) this.value[name] = value
+  submit = async () => {
+    if (this.disabled === true) {
+      this.validateAll()
+    } else {
+      console.log(this.value)
+
+      this.setAlert('progress', 'Download...')
+
+      try {
+        const res = await fetch('/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: this.convertData(),
+        })
+
+        const data = await res.json()
+
+        if (res.ok) {
+          this.setAlert('success', data.message)
+        } else {
+          this.setAlert('error', data.message)
+        }
+      } catch (error) {
+        this.setAlert('error', error.message)
+      }
+    }
+  }
+
+  convertData = () => {
+    return JSON.stringify({
+      [this.FIELD_NAME.EMAIL]:
+        this.value[this.FIELD_NAME.EMAIL],
+      [this.FIELD_NAME.PASSWORD]:
+        this.value[this.FIELD_NAME.PASSWORD],
+      [this.FIELD_NAME.ROLE]:
+        this.value[this.FIELD_NAME.ROLE],
+    })
   }
 }
 
-window.signupForm = SignupForm
+window.signupForm = new SignupForm()
